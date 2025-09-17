3187c87d0a3e1ade27f6c460593ec08e5432853d
diljithmon170 <diljithmon170@gmail.com>
2025-09-17 13:01:39 +0530

    added opencv project

--- ml/opencv/actors_img/img_class.py
+++ ml/opencv/actors_img/img_class.py
@@ -6,5 +6,18 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('./img/surya/s1.jpg')
 img.shape
-plt.imshow(img)
+# plt.imshow(img)
+# plt.show()
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray.shape
+
+face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('./haarcascade_eye.xml')
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+(x,y,w,h) = faces[0]
+
+face_img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+plt.imshow(face_img)
 plt.show()

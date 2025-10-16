e8cbd91f89122f5880a56a7f5f264c768227f89f
diljithmon170 <diljithmon170@gmail.com>
2025-10-16 20:31:20 +0530

    dl completed

--- dl/project/api/main.py
+++ dl/project/api/main.py
@@ -7,7 +7,8 @@ import tensorflow as tf
 
 app = FastAPI()
 
-MODEL = tf.keras.models.load_model("C:\Users\dilji\Documents\AI-Engineer\dl\project\saved_models\1")
+# MODEL = tf.keras.models.load_model("C:\Users\dilji\Documents\AI-Engineer\dl\project\saved_models\1")
+MODEL = tf.keras.models.load_model("C:/Users/dilji/Documents/AI-Engineer/dl/project/saved_models/1")
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 @app.get("/ping")
 async def ping():
